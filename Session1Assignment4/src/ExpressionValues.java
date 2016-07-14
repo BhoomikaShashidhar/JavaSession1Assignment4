1dc992ea5451411645ee6c479f7339cf07234320
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-14 10:50:10 +0530

    Add files via upload

---
+++ Session1Assignment4/src/ExpressionValues.java
@@ -0,0 +1,14 @@
+
+public class ExpressionValues {
+	public static void main (String args[]){
+		int x=5,y=10,a,b,c,d;
+		a= x + y * 2;
+		b= x - y + 2;
+		c= (x+y) * 2;
+		d= y % x;
+	System.out.println("value of x+y*2=  "+a);
+	System.out.println("value of x-y+2=  "+b);
+	System.out.println("value of (x+y)*2=  "+c);
+	System.out.println("value of y%x=  "+d);
+	}
+}
